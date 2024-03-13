48f91611c58645da6ffaf7c9c70686c16046831a
George <g.m.6@live.com>
2024-03-13 19:53:22 +0200

    todos crud in progress

---
+++ api/get_todos.php
@@ -0,0 +1,27 @@
+<?php
+include('connection.php');
+
+$todo_id = $_GET('todo_id');
+$user_id = $_GET('user_id');
+
+$query = $mysqli->prepare('SELECT * FROM todos WHERE id=? AND user_id=?');
+$query->bind_param('ii',$todo_id,$user_id);
+$query->execute();
+$query->store_result();
+$num_rows = $query->num_rows();
+
+if ($num_rows > 0) {
+    $todos = [];
+    $query->bind_result($todo_id,$todo_description,$user_id);
+    while ($query->fetch()) {
+        $todo = [
+            'id'=>$todo_id,
+            'description'=>$todo_description,
+            'user_id'=>$user_id
+        ];
+        $todos[] = $todo;
+    }
+    $response['Status'] = 'Success';
+    $response['Todos'] = $todos;
+}
+echo json_encode($response);
---
+++ api/post_todo.php
@@ -0,0 +1,28 @@
+<?php
+include 'connection.php';
+
+$user_id = $_POST['user_id'];
+$todo_id = $_POST['todo_id'];
+$todo_description = $_POST['todo_description'];
+
+$check_existing_id = $mysqli->prepare('SELECT id FROM todos WHERE id=?');
+$check_existing_id->bind_param('i', $todo_id);
+$check_existing_id->execute();
+$check_existing_id->store_result();
+$id_exists = $check_existing_id->num_rows();
+
+if ($id_exists > 0) {
+    $response['Status'] = 'Todo already exists, overwriting changes';
+    $query = $mysqli->prepare('UPDATE todos SET description=?');
+    $query->bind_param('s', $todo_description);
+    $query->execute();
+    $response['Message'] = 'Todo $todo_id was updated successfully';
+} else {
+    $response['Status'] = 'Adding new todo';
+    $query = $mysqli->prepare(
+        'INSERT INTO todos(id,description,user_id) VALUES(?,?,?)'
+    );
+    $query->bind_param('isi', $todo_id, $todo_description, $user_id);
+    $query->execute();
+    $response['Message'] = 'Todo $todo_id for user id: $user_id was created';
+}
